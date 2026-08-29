777febd09d3e40f48f4aebd197db73ebbfc27137
ds.kim <ollybaysion@gmail.com>
2026-08-29 21:52:11 +0900

    chore: Astro 블로그 스캐폴드 초기 커밋

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'renoir.log';
+export const SITE_DESCRIPTION = '기술과 커피를 기록하는 블로그';
